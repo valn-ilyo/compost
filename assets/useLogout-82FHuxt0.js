18d4d393cc2eed7dae98b7e5e3b7fa217242c0dc
Vivian Alexander Lyngdoh Nonglait <valnilyo@gmail.com>
2026-05-03 21:16:44 +0530

    Updates

--- assets/useLogout-82FHuxt0.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./supabaseClient-HTjMTI1T.js";import{f as t,i as n,n as r,o as i}from"./index-CIB3_u6V.js";function a(){let a=n();return{logout:async(n=`/auth`)=>{await e.auth.signOut(),r().reset(),i().clearAll(),t().$reset(),await a.push(n)}}}export{a as t};
